db565eabd7be4f7419631a049a11805089cc8b88
ErrorNotFound <benedikt.jakobs@outlook.de>
2025-08-01 13:32:36 +0200

    Add new Test project

---
+++ ImbaBetWeb.Test/Validation/BetValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using ImbaBetWeb.Models;
+using ImbaBetWeb.Validation;
+
+namespace ImbaBetWeb.Test.Validation
+{
+    public class BetValidatorTests
+    {
+        private BetValidator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new BetValidator();
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(10, 10)]
+        public void BetValidator_ShouldPass_WhenGoalCountReasonable(int goalA, int goalB)
+        {
+            // Arrange
+            var bet = new Bet() { UserId = string.Empty, GoalsA = goalA, GoalsB = goalB };
+
+            // Act
+            var result = validator.Validate(bet);
+
+            // Assert
+            Assert.That(result.IsValid, Is.True);
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(999, 0)]
+        [TestCase(0, 999)]
+        public void BetValidator_ShouldFail_WhenGoalCountUnreasonable(int goalA, int goalB)
+        {
+            // Arrange
+            var bet = new Bet() { UserId = string.Empty, GoalsA = goalA, GoalsB = goalB };
+
+            // Act
+            var result = validator.Validate(bet);
+
+            // Assert
+            Assert.That(result.IsValid, Is.False);
+        }
+    }
+}
---
+++ ImbaBetWeb.Test/Validation/CommunityNameValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using ImbaBetWeb.Validation;
+
+namespace ImbaBetWeb.Test.Validation
+{
+    public class CommunityNameValidatorTests
+    {
+        private CommunityNameValidator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            var existingNames = new List<string>()
+            {
+                "NameA",
+                "NameB",
+                "NameC"
+            };
+
+            validator = new CommunityNameValidator(existingNames);
+        }
+
+        [TestCase("abc")]
+        public void CommunityNameValidator_ShouldPass_WhenGoalCountReasonable(string nameUnderTest)
+        {
+            // Act
+            var result = validator.Validate(nameUnderTest);
+
+            // Assert
+            Assert.That(result.IsValid, Is.True);
+        }
+
+        [TestCase("")] // empty
+        [TestCase("ab")] // length under 3
+        [TestCase("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")] // length over 30
+        public void CommunityNameValidator_ShouldFail_WhenGivenNameUnreasonable(string nameUnderTest)
+        {
+            // Act
+            var result = validator.Validate(nameUnderTest);
+
+            // Assert
+            Assert.That(result.IsValid, Is.False);
+        }
+
+        [Test]
+        public void CommunityNameValidator_ShouldFail_WhenGivenNameAlreadyUsed()
+        {
+            // Act
+            var result = validator.Validate("NameA");
+
+            // Assert
+            Assert.That(result.IsValid, Is.False);
+        }
+    }
+}

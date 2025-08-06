af1eb8cbd11dc19fa2a80c1cd3666c575415f677
ErrorNotFound <benedikt.jakobs@outlook.de>
2025-08-06 23:13:52 +0200

    Add TeamStore and tests

---
+++ ImbaBetWeb.Tests.Int/DataAccess/TeamStoreTests.cs
@@ -0,0 +1,56 @@
+﻿using Allure.NUnit;
+using ImbaBetWeb.DataAccess;
+using ImbaBetWeb.Model;
+using ImbaBetWeb.Tests.Int.DataAccess.TestHelper;
+
+namespace ImbaBetWeb.Tests.Int.DataAccess
+{
+    [AllureNUnit]
+    public class TeamStoreTests : DataAccessTestsBase
+    {
+        [Test]
+        public async Task EnsureInitializedAsync_NoTableAvailable_TableIsAdded()
+        {
+            // Arrange
+            var store = new TeamStore(TestDatabase.ConnectionString);
+
+            // Act and Assert
+            Assert.That(await TestDatabase.TableExistsAsync(store.TableName), Is.False);
+            await store.EnsureInitializedAsync();
+            Assert.That(await TestDatabase.TableExistsAsync(store.TableName), Is.True);
+        }
+
+        [Test]
+        public async Task CreateGetUpdateDelete_TestWorkflow_NoUnexpectedError()
+        {
+            // Arrange
+            var store = new TeamStore(TestDatabase.ConnectionString);
+            await store.EnsureInitializedAsync();
+            var team = new NTeam()
+            {
+                Name = "name",
+                FlagCountryCode = null,
+                StackRank = 1
+            };
+
+            // Test Create and Retrieve
+            team.Id = await store.CreateAsync(team);
+            var retrieved = await store.GetAsync(team.Id);
+            Assert.That(retrieved, Is.EqualTo(team));
+
+            // Test Update
+            team.Name = "new name";
+            team.FlagCountryCode = "de";
+            team.StackRank = 2;
+            await store.UpdateAsync(team);
+            retrieved = (await store.GetAllAsync()).Single();
+            Assert.That(retrieved, Is.EqualTo(team));
+
+            // Test Delete
+            await store.DeleteAsync(team);
+            Assert.ThrowsAsync<InvalidOperationException>(() => store.GetAsync(team.Id));
+            var items = await store.GetAllAsync();
+            Assert.That(items, Is.Empty);
+        }
+    }
+}
--- ImbaBetWeb/DataAccess/Interfaces/ITeamStore.cs
+++ ImbaBetWeb/DataAccess/Interfaces/ITeamStore.cs
@@ -4,5 +4,12 @@ namespace ImbaBetWeb.DataAccess.Interfaces
 {
     public interface ITeamStore
     {
+        Task<int> CreateAsync(NTeam obj);
+        Task<NTeam> GetAsync(int id);
+        Task<IEnumerable<NTeam>> GetAllAsync();
+        Task UpdateAsync(NTeam obj);
+        Task DeleteAsync(NTeam id);
+
+        Task EnsureInitializedAsync();
     }
 }
